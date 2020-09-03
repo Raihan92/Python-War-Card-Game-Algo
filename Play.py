c8a4a78ac27351c998dd1a7059b2157e4365bb85
Dewan Mahmud Raihan <Raihan92@users.noreply.github.com>
2020-09-03 17:36:47 +0600

    Update Play.py

--- Play.py
+++ Play.py
@@ -9,93 +9,95 @@ for i in range(26):
     player2.add_cards(new_deck.pop_one())
 
 print('Player {} has cards now: {}'.format(player1.name, len(player1.all_cards)))
-print('Player {} has cards now:def Play_War_Card_Game():
-    game_on = True
-    
-    round_number = 0
-    
-    while game_on:
+print('Player {} has cards now: {}'.format(player2.name, len(player2.all_cards)))
 
-        player_one_cards_rem = len(player1.all_cards)
-        player_two_cards_rem = len(player2.all_cards)
+def Play_War_Card_Game():
+game_on = True
 
-        if player_one_cards_rem == 0 and player_two_cards_rem != 0:
-            print('{} wins the game. Congratulations!'.format(player2.name))
-            game_on = False
-            break
-        elif player_one_cards_rem != 0 and player_two_cards_rem == 0:
-            print('{} wins the game. Congratulations!'.format(player1.name))
-            game_on = False
-            break
-        elif player_one_cards_rem == 0 and player_two_cards_rem == 0:
-            print('Match draw! Play again')
-            game_on = False
-            break
+round_number = 0
 
-        round_number += 1
-        print('Round Number: {}'.format(round_number))
+while game_on:
 
-        if game_on:
+    player_one_cards_rem = len(player1.all_cards)
+    player_two_cards_rem = len(player2.all_cards)
 
-            player_one_card = []
-            player_two_card = []
-            
-            player_one_card.append(player1.pop_one())
-            player_two_card.append(player2.pop_one())
+    if player_one_cards_rem == 0 and player_two_cards_rem != 0:
+        print('{} wins the game. Congratulations!'.format(player2.name))
+        game_on = False
+        break
+    elif player_one_cards_rem != 0 and player_two_cards_rem == 0:
+        print('{} wins the game. Congratulations!'.format(player1.name))
+        game_on = False
+        break
+    elif player_one_cards_rem == 0 and player_two_cards_rem == 0:
+        print('Match draw! Play again')
+        game_on = False
+        break
 
-            war = False
-            if player_one_card[0].value == player_two_card[0].value:
-                war = True
+    round_number += 1
+    print('Round Number: {}'.format(round_number))
 
-            if war:
+    if game_on:
 
-                while war:
+        player_one_card = []
+        player_two_card = []
 
-                    print(' --- War begins --- ')
+        player_one_card.append(player1.pop_one())
+        player_two_card.append(player2.pop_one())
 
-                    if len(player1.all_cards) == 0 or len(player1.all_cards) < 5:
-                        print('{} wins the game. Congratulations!'.format(player2.name))
-                        game_on = False
-                        war = False
-                        break
-                    if len(player2.all_cards) == 0 or len(player2.all_cards) < 5:
-                        print('{} wins the game. Congratulations!'.format(player1.name))
-                        game_on = False
-                        war = False
-                        break
-                    if len(player1.all_cards) == 0 and len(player2.all_cards) == 0:
-                        print('Match draw! Play again')
-                        game_on = False
-                        war = False
-                        break
+        war = False
+        if player_one_card[0].value == player_two_card[0].value:
+            war = True
 
-                    for i in range(5):
-                        player_one_card.append(player1.pop_one())
-                        player_two_card.append(player2.pop_one())
+        if war:
 
-                    if player_one_card[-1].value < player_two_card[-1].value:
-                        player2.add_cards(player_one_card)
-                        player2.add_cards(player_two_card)
-                        war = False
-                    elif player_one_card[-1].value > player_two_card[-1].value:
-                        player1.add_cards(player_two_card)
-                        player1.add_cards(player_one_card)
-                        war = False
-                    elif player_one_card[-1].value == player_two_card[-1].value:
-                        pass
+            while war:
 
-                    print(' ----- War ends ----- ')
+                print(' --- War begins --- ')
 
-            else:
-                if player_one_card[0].value < player_two_card[0].value:
+                if len(player1.all_cards) == 0 or len(player1.all_cards) < 5:
+                    print('{} wins the game. Congratulations!'.format(player2.name))
+                    game_on = False
+                    war = False
+                    break
+                if len(player2.all_cards) == 0 or len(player2.all_cards) < 5:
+                    print('{} wins the game. Congratulations!'.format(player1.name))
+                    game_on = False
+                    war = False
+                    break
+                if len(player1.all_cards) == 0 and len(player2.all_cards) == 0:
+                    print('Match draw! Play again')
+                    game_on = False
+                    war = False
+                    break
+
+                for i in range(5):
+                    player_one_card.append(player1.pop_one())
+                    player_two_card.append(player2.pop_one())
+
+                if player_one_card[-1].value < player_two_card[-1].value:
                     player2.add_cards(player_one_card)
                     player2.add_cards(player_two_card)
-                else:
-                    player1.add_cards(player_one_card)
+                    war = False
+                elif player_one_card[-1].value > player_two_card[-1].value:
                     player1.add_cards(player_two_card)
+                    player1.add_cards(player_one_card)
+                    war = False
+                elif player_one_card[-1].value == player_two_card[-1].value:
+                    pass
 
-            print('{}'.format(player1, len(player1.all_cards)))
-            print('{}'.format(player2, len(player2.all_cards)))
+                print(' ----- War ends ----- ')
+
         else:
-            pass {}'.format(player2.name, len(player2.all_cards)))
+            if player_one_card[0].value < player_two_card[0].value:
+                player2.add_cards(player_one_card)
+                player2.add_cards(player_two_card)
+            else:
+                player1.add_cards(player_one_card)
+                player1.add_cards(player_two_card)
+
+        print('{}'.format(player1, len(player1.all_cards)))
+        print('{}'.format(player2, len(player2.all_cards)))
+    else:
+        pass {}'.format(player2.name, len(player2.all_cards)))
 
